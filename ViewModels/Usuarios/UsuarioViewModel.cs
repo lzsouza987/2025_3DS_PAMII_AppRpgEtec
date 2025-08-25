077ad5ded4910b442ba56a01be91e7451c11b157
etechas <etechas@LI02-21>
2025-08-25 08:30:43 -0300

    Add project files.

---
+++ ViewModels/Usuarios/UsuarioViewModel.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using AppRpgEtec.Models;
+using AppRpgEtec.Services.Usuarios;
+
+namespace AppRpgEtec.ViewModels.Usuarios
+{
+    public class UsuarioViewModel : BaseViewModel
+    {
+        public UsuarioViewModel()
+        {
+            uService = new UsuarioService();
+            InicializarCommands();
+        }
+
+        public void InicializarCommands()
+        {
+            AutenticarCommand = new Command(async () => await AutenticarUsuario());
+        }
+
+        private UsuarioService uService;
+        public ICommand AutenticarCommand { get; set; }
+       
+
+        //Meu IP inletex 192.168.2.167
+        #region AtributosPropriedades
+        private string login = string.Empty;
+        private string senha = string.Empty;
+
+        public string Login 
+        { 
+            get => login;
+            set 
+            { 
+                login = value;
+                OnPropertyChanged();
+            } 
+        }
+        public string Senha 
+        { 
+            get => senha;
+            set 
+            { 
+                senha = value; 
+                OnPropertyChanged();
+            } 
+        }
+
+        #endregion
+
+        #region Metodos
+        public async Task AutenticarUsuario()
+        {
+            try
+            {
+                Usuario u = new Usuario();
+                u.Username = login;
+                u.PasswordString = senha;
+
+                Usuario uAutenticado = await uService.PostAutenticarUsuarioAsync(u);
+
+                if(!string.IsNullOrEmpty(uAutenticado.Token))
+                {
+                    string mensagem = $"Bem-vindo(a) {uAutenticado.Username}";
+
+                    //Guardando dados para uso futuro
+                    Preferences.Set("UsuarioId", uAutenticado.Id);
+                    Preferences.Set("UsuarioUsername", uAutenticado.Username);
+                    Preferences.Set("UsuarioPerfil", uAutenticado.Perfil);
+                    Preferences.Set("UsuarioToken", uAutenticado.Token);
+
+                    await Application.Current.MainPage
+                        .DisplayAlert("Informação", mensagem, "Ok");
+
+                    Application.Current.MainPage = new MainPage();
+                }
+                else
+                {
+                    await Application.Current.MainPage
+                        .DisplayAlert("Informação", "Dados incorretos 😑 ", "Ok");
+                }
+            }
+            catch (Exception ex)
+            {
+                await Application.Current.MainPage
+                    .DisplayAlert("Informação", ex.Message + ex.InnerException, "Ok");
+            }            
+        }
+        #endregion
+    }
+}
